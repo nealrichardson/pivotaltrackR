61f1924cd71f079c179ef071b11178af0ab76f61
Neal Richardson <neal.p.richardson@gmail.com>
2018-06-10 21:25:47 -0700

    Redacting request headers isn't necessary with the next release of httptest

--- inst/httptest/redact.R
+++ inst/httptest/redact.R
@@ -1,7 +1,6 @@
 function (response) {
     require(magrittr, quietly=TRUE)
     response %>%
-        redact_headers("X-TrackerToken") %>%
         gsub_response("https://www.pivotaltracker.com/services/v5/", "", fixed=TRUE) %>%
         gsub_response(getOption("pivotal.project"), "123")
 }
